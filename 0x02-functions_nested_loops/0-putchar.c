f798e39f9101eb8e919f28bfc95b4bd114c9d1bf
S-Balami <Balamistephen080@gmail.com>
2022-03-16 15:00:09 -0700

    Prints

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - program starts and ends here
+ * return: 0 on success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is all about funtions and how to use them
