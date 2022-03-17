e42a2a761caa31bee2a3b2e6af6c75cd9fd1a63b
S-Balami <Balamistephen080@gmail.com>
2022-03-17 10:40:31 -0700

    Check for the uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_isuppert(int c) - checks for uppercase
+ *Returns: 0 if lowercase else 1
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A')) && ((c <= 'Z'));
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This will take us more further into functions
